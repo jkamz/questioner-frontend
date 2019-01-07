406f89e882a4b7cdcca87de3d0686bb7d593f987
jkamz <jkamz6432@gmail.com>
2019-01-07 13:17:23 +0300

    [starts162998304] Add delete buttons and enable js to restrict users

--- UI/static/js/script.js
+++ UI/static/js/script.js
@@ -1,14 +1,17 @@
-
-
 function hideCreateButton(argument) {
     x = localStorage.getItem('userName');
     document.getElementById('user').innerHTML = x;
 
     var user = document.getElementById('user').innerHTML;
     var status = document.getElementById('meet-button')
+    var status2 = document.getElementById('delete-meet-button')
 
     if (user !== 'Admin') {
-        status.style.display = 'none'
+        status.style.display = 'none';
+
+        [].forEach.call(document.querySelectorAll('.delete-meetup-button'), function(el) {
+            el.style.visibility = 'hidden';
+        });
     }
 
 
@@ -29,11 +32,11 @@ function logInAdmin(argument) {
 
     if (checkBox.checked == true) {
 
-        localStorage.setItem("userName",input);
+        localStorage.setItem("userName", input);
 
     } else {
 
-        localStorage.setItem("userName",input2);
+        localStorage.setItem("userName", input2);
     }
 }
 
