edf0eb029cc8af63a39a8534f48f198d6dd4e70f
jkamz <jkamz6432@gmail.com>
2019-02-09 13:42:44 +0300

    [feature#163785844] get location and date from API

--- UI/static/js/index.js
+++ UI/static/js/index.js
@@ -1,6 +1,8 @@
 function upcomingMeetups() {
     const url = 'https://questionerandela.herokuapp.com/api/v2/meetups/upcoming';
     let titles = document.getElementsByClassName('meetuptitle');
+    let locations = document.getElementsByClassName('location');
+    let dates = document.getElementsByClassName('date');
 
     fetch(url, {
         method: 'GET',
@@ -16,6 +18,8 @@ function upcomingMeetups() {
                 for (let x = 0; x < meetups.length; x++) {
                     for (let i = 0; i < titles.length; i++) {
                         titles.item(i).innerHTML = meetups[i].topic;
+                        locations.item(i).innerHTML = `Location: ${meetups[i].location}`;
+                        dates.item(i).innerHTML = `Date: ${meetups[i].happeningon}`;
                     }
                 }
             }
