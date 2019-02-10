c4a96131212453dc142c787a25119029896e1098
jkamz <jkamz6432@gmail.com>
2019-02-10 03:05:05 +0300

    [chore#163844625] fetch meetup ID for each meeting in homepage

--- UI/static/js/index.js
+++ UI/static/js/index.js
@@ -1,9 +1,20 @@
+// Handle logic for getting upcoming meetups and specific meetup ID
+
+// Save meetup ID to help visit specific meetup page
+const meetupDetails = (id) => {
+    // event.preventDefault();
+    // event.stopPropagation();
+    localStorage.setItem('meetup_id', id);
+    window.location.href = 'meetup.html';
+};
+
+// fetch upcoming meetups and render to html
 function upcomingMeetups() {
     const url = 'https://questionerandela.herokuapp.com/api/v2/meetups/upcoming';
-    let titles = document.getElementsByClassName('meetuptitle');
-    let locations = document.getElementsByClassName('location');
-    let dates = document.getElementsByClassName('date');
-    let meetupsdiv = document.getElementsByClassName('meetup');
+    const titles = document.getElementsByClassName('meetuptitle');
+    const locations = document.getElementsByClassName('location');
+    const dates = document.getElementsByClassName('date');
+    const meetupsdiv = document.getElementsByClassName('box');
 
     fetch(url, {
         method: 'GET',
@@ -14,18 +25,23 @@ function upcomingMeetups() {
         .then(response => response.json())
         .then((data) => {
             if (data.message === 'success') {
-                let meetups = data.meetups;
-                meetups.reverse();
-                for (let x = 0; x < meetups.length; x++) {
-                    for (let i = 0; i < titles.length; i++) {
-                        titles.item(i).innerHTML = meetups[i].topic;
-                        locations.item(i).innerHTML = `location: ${meetups[i].location}`;
-                        dates.item(i).innerHTML = `Happening On: ${meetups[i].happeningon}`;
-                        meetupsdiv.item(i).setAttribute('id', meetups[i].meetup_id);
-                    }
+                const meetupsArr = data.meetups;
+                meetupsArr.reverse();
+                // for (let x = 0; x < meetupsArr.length; x += 1) {
+                for (let i = 0; i < titles.length; i += 1) {
+                    titles.item(i).innerHTML = meetupsArr[i].topic;
+                    locations.item(i).innerHTML = `location: ${meetupsArr[i].location}`;
+                    dates.item(i).innerHTML = `Happening On: ${meetupsArr[i].happeningon}`;
+                    meetupsdiv.item(i).setAttribute('id', meetupsArr[i].meetup_id);
+                    document.getElementById(meetupsArr[i].meetup_id).addEventListener('click', () => { meetupDetails(meetupsArr[i].meetup_id); });
                 }
+                // }
+                // for (let meetup of meetupsArr) {
+                //     document.getElementById(meetup.meetup_id).addEventListener('click', meetupDetails);
+            // }
             }
         });
 }
 
+// add event listener
 window.addEventListener('load', upcomingMeetups);
