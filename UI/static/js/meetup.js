c036a7530ffb31222fe114235c665a651a4c53f0
jkamz <jkamz6432@gmail.com>
2019-02-13 14:27:13 +0300

    [chore#1163844625] fetch get meetup questions api

--- UI/static/js/meetup.js
+++ UI/static/js/meetup.js
@@ -4,6 +4,7 @@ const meetupId = localStorage.getItem('meetup_id');
 function Meetup() {
 
     const url = `https://questionerandela.herokuapp.com/api/v2/meetups/${meetupId}`;
+    const questionUrl = `https://questionerandela.herokuapp.com/api/v2/meetups/${meetupId}/questions`;
     const title = document.getElementById('title');
     const host = document.getElementById('host');
     const summary = document.getElementById('summary');
@@ -24,6 +25,41 @@ function Meetup() {
                 host.innerHTML = meetupObj.host;
                 summary.innerHTML = meetupObj.summary;
                 date.innerHTML = dateFormated;
+                return fetch(questionUrl, {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                })
+                    .then(response => response.json())
+                    .then((data1) => {
+                        if (data1.message === 'success') {
+                            const questionObj = data1.questions;
+                            console.log(questionObj);
+                            result = '';
+                            for (let item of questionObj) {
+                                result +=
+                                `<div class="question">
+                                    <div class="question-no">Q.1</div>
+                                    <div class="upvotes">
+                                        <a href="#"><div class="arrow up"></div></a>
+                                        <div class="votes">${item.votes}</div>
+                                        <a href="#"><div class="arrow down"></div></a>
+                                    </div>
+                                    <div class="question-body">
+                                        <a href="comments.html" class="question-details">${item.body}</a>
+                                        <p>Submitted by <a href="#" class="submittedby">${item.author}</a> at 12:23pm</p>
+                                        <div class="question-options">
+                                            <span><a href="comments.html">5 Comments</a></span>
+                                            <span>Report</span>
+                                        </div>
+                                    </div>
+                                </div>`
+                            }
+                            document.getElementById('results').innerHTML = result;
+                        }
+                    });
+
             } else {
                 window.alert(data.message);
             }
