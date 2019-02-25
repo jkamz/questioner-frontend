3b4e582ab3863ad2e8715e81199822e5b58af284
jkamz <jkamz6432@gmail.com>
2019-02-25 03:39:28 +0300

    [feature#164131692] add method to fectch comments

--- UI/static/js/comments.js
+++ UI/static/js/comments.js
@@ -1,6 +1,20 @@
+const questionId = localStorage.getItem('question_id');
 const form = document.querySelector('form');
 const spinner = document.querySelector('.comments-loading');
+const commentsUrl = `https://questionerandela.herokuapp.com/api/v2/questions/${questionId}/comments`;
+spinner.style.display = '';
 
-spinner.style.display = 'none';
+getComments();
 
-form.addEventListener()
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+});
+
+function getComments() {
+    fetch(commentsUrl)
+        .then(response => response.json())
+        .then((data) => {
+            const commentsObj = data.comments;
+            console.log(commentsObj);
+        });
+}
